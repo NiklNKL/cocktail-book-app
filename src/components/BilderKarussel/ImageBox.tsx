7598b49472da5704ea0fe67eae03111b49851dad
NiklNKL <janniklas-ewert@web.de>
2023-03-04 23:17:57 +0100

    Added Text to pictures

--- src/components/BilderKarussel/ImageBox.tsx
+++ src/components/BilderKarussel/ImageBox.tsx
@@ -1,31 +1,37 @@
 import { Box } from "@mui/material";
+import "./hover.css";
 
 export default function ImageBox({ source, alt }) {
   return (
-    <Box display="flex">
-      {/* <Box
-        display="flex"
-        sx={{
-          opacity: "0.0",
-          "&:hover": {
-            backgroundColor: "primary.main",
-            opacity: "1",
-          },
-        }}
-      >
-        <h1>Test</h1>
-      </Box> */}
-      <Box
-        display="flex"
-        sx={{
-          "&:hover": {
-            backgroundColor: "primary.main",
-            opacity: [0.7, 0.6, 0.5],
-          },
-        }}
-      >
-        <img src={source} alt={alt}></img>
-      </Box>
+    // <Box display="flex">
+    //   {/* <Box
+    //     display="flex"
+    //     sx={{
+    //       opacity: "0.0",
+    //       "&:hover": {
+    //         backgroundColor: "primary.main",
+    //         opacity: "1",
+    //       },
+    //     }}
+    //   >
+    //     <h1>Test</h1>
+    //   </Box> */}
+    //   <Box
+    // //     display="flex"
+    // //     sx={{
+    // //       "&:hover": {
+    // //         backgroundColor: "primary.main",
+    // //         opacity: [0.7, 0.6, 0.5],
+    // //       },
+    // //     }}
+    // //   >
+    //     <img src={source} alt={alt}></img>
+    //   </Box>
+    // </Box>
+
+    <Box className="container">
+      <img src={source} alt={alt} className="image" />
+      <Box className="overlay">{alt}</Box>
     </Box>
   );
 }
