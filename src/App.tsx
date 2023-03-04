e1b21f560670738a8f7f8427ce540243a7a3fcbf
DevelopingNacho <dominik.ruth@edu.fhdw.de>
2023-03-04 22:17:44 +0100

    Added a SideBar

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import SignInSide from "./components/SideBar";
 import MenuAppBar from "./components/AppBar/AppBar";
 import AppAppBar from "./components/AppAppBar";
 import Images from "./components/Images";
+
 function App() {
   return (
     <Box height="100vh">
       <AppAppBar />
-
       <Box
         justifyContent="center"
         alignItems="center"
