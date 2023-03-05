a5d686a74a3484a5ddbdb1f019037d417df479bb
DevelopingNacho <dominik.ruth@edu.fhdw.de>
2023-03-05 18:25:52 +0100

    Added IngredientsPage & RandomCocktailPage with API Request

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import CocktailList from "./components/Pages/CocktailList";
+import CocktailList from "./components/Pages/RandomCocktailsPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/Pages/HomePage";
+import IngredientsPage from "./components/Pages/IngredientsPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/allcocktails" element={<CocktailList />} />
+      <Route path="/ingredients" element={<IngredientsPage />} />
     </Routes>
   );
 }
