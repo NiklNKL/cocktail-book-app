41a85e6b78b20eba4303f2699f26b0864d98dc59
DevelopingNacho <dominik.ruth@edu.fhdw.de>
2023-03-05 01:20:32 +0100

    Corrected a wrong import in the App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { AppBar, Box, Button, Grid } from "@mui/material";
 import SignInSide from "./components/SideBar";
-import MenuAppBar from "./AppBar/AppBar";
 import Images from "./components/BilderKarussel/Images";
 import AppBarElement from "./components/AppBarElement";
 function App() {
