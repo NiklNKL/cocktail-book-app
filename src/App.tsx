81234fc24622b231cd8800317a51c3a684b4bd6a
DevelopingNacho <dominik.ruth@edu.fhdw.de>
2023-03-05 01:19:37 +0100

    Adjusted the appbar and added a searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Box, Button, Grid } from "@mui/material";
+import { AppBar, Box, Button, Grid } from "@mui/material";
 import SignInSide from "./components/SideBar";
-import MenuAppBar from "./components/AppBar/AppBar";
-import AppAppBar from "./components/AppAppBar";
+import MenuAppBar from "./AppBar/AppBar";
 import Images from "./components/BilderKarussel/Images";
+import AppBarElement from "./components/AppBarElement";
 function App() {
   return (
     <Box height="100vh">
-      <AppAppBar />
+      <AppBarElement />
       <Box
         justifyContent="center"
         alignItems="center"
