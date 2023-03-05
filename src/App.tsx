6fdc8b1895aa6fadec5eef669b25b8bb79bcaafb
NiklNKL <janniklas-ewert@web.de>
2023-03-05 18:57:55 +0100

    Added dynamic pages for drinks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import CocktailList from "./components/Pages/CocktailList";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/Pages/HomePage";
-
+import DrinkPage from "./components/Pages/DrinkPage";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/allcocktails" element={<CocktailList />} />
+      <Route
+        path="/cocktail/:name"
+        element={<DrinkPage name={window.location.pathname.split("/")[2]} />}
+      />
     </Routes>
   );
 }
