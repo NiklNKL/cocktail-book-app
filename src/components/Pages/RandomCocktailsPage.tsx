f2473bfd0bc5d3a4114d471963e34010dda436f3
DevelopingNacho <dominik.ruth@edu.fhdw.de>
2023-03-06 10:37:57 +0100

    Added data displaying to the allcocktails and ingredients page

--- src/components/Pages/RandomCocktailsPage.tsx
+++ src/components/Pages/RandomCocktailsPage.tsx
@@ -1,66 +1,93 @@
-import { Box, ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
+import {
+  Box,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  Toolbar,
+} from "@mui/material";
 import AppBarElement from "../AppBarElement";
-import { useLocation } from "react-router-dom";
 import {
   JSXElementConstructor,
   Key,
   ReactElement,
   ReactFragment,
   ReactPortal,
+  useEffect,
   useState,
 } from "react";
+import axios from "axios";
 
-async function CocktailList() {
-  const [data, setData] = useState(null);
+function CocktailList() {
+  const [data, setData] = useState([]);
 
-  const response = await fetch(
-    "https://heavydrinking.herokuapp.com/random_list?startAt=0&numResults=5"
-  )
-    .then((response) => response.json())
-    .then((response) => setData(response));
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          "https://api.smartinies.recipes/random_list?startAt=0&numResults=5"
+        );
+        setData(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
 
+    fetchData();
+  }, []);
   return (
     <Box height="100vh">
       <AppBarElement />
-
-      <ImageList sx={{ width: 500, height: 450 }}>
-        {data.map(
-          (item: {
-            img: Key | null | undefined;
-            image: any;
-            cocktailName:
-              | string
-              | number
-              | boolean
-              | ReactElement<any, string | JSXElementConstructor<any>>
-              | ReactFragment
-              | null
-              | undefined;
-            instructions:
-              | string
-              | number
-              | boolean
-              | ReactElement<any, string | JSXElementConstructor<any>>
-              | ReactFragment
-              | ReactPortal
-              | null
-              | undefined;
-          }) => (
-            <ImageListItem key={item.img}>
-              <img
-                src={`${item.image}?w=248&fit=crop&auto=format`}
-                srcSet={`${item.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                loading="lazy"
-              />
-              <ImageListItemBar
-                title={item.cocktailName}
-                subtitle={<span>Instructions: {item.instructions}</span>}
-                position="below"
-              />
-            </ImageListItem>
-          )
-        )}
-      </ImageList>
+      <Toolbar />
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        marginLeft={"50px"}
+        marginRight={"50px"}
+      >
+        <ImageList
+          sx={{ display: "inline", marginTop: "20px" }}
+          rowHeight="auto"
+        >
+          {data.map(
+            (item: {
+              id: Key | null | undefined;
+              img: Key | null | undefined;
+              image: any;
+              cocktailName:
+                | string
+                | number
+                | boolean
+                | ReactElement<any, string | JSXElementConstructor<any>>
+                | ReactFragment
+                | null
+                | undefined;
+              instructions:
+                | string
+                | number
+                | boolean
+                | ReactElement<any, string | JSXElementConstructor<any>>
+                | ReactFragment
+                | ReactPortal
+                | null
+                | undefined;
+            }) => (
+              <ImageListItem key={item.id} sx={{ width: "auto" }}>
+                <img
+                  src={`${item.image}?w=248&fit=crop&auto=format`}
+                  srcSet={`${item.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  loading="lazy"
+                />
+                <ImageListItemBar
+                  title={item.cocktailName}
+                  subtitle={<span>Instructions: {item.instructions}</span>}
+                  position="below"
+                />
+              </ImageListItem>
+            )
+          )}
+        </ImageList>
+      </Box>
     </Box>
   );
 }
