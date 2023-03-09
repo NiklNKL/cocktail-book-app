46a4ca386d83fce37a39280dff750a889e2157fd
DevelopingNacho <104835849+DevelopingNacho@users.noreply.github.com>
2023-03-09 22:13:03 +0100

    Added fonts and designed the drink page

--- src/components/Pages/DrinkPage.tsx
+++ src/components/Pages/DrinkPage.tsx
@@ -1,14 +1,23 @@
 import { Box, Grid, makeStyles, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
 import AppBarElementNoSearch from "../AppBarNoSearch/AppBarElementNoSearch";
-
+import { grey } from "@mui/material/colors";
 import axios from "axios";
+import { Paper } from "@material-ui/core";
+import Image from "../../bar_background.jpg";
+import "../../../fonts/fonts.css";
 
 interface IngredientGridProps {
   ingredientImage: string[];
   ingredient: string[];
   ingredientMeasure: string[];
 }
+
+const ingredientStyle = {
+  width: "40%",
+  height: "40%",
+  objectFit: "cover",
+};
 
 const IngredientGrid: React.FC<IngredientGridProps> = ({
   ingredientImage,
@@ -26,12 +35,12 @@ const IngredientGrid: React.FC<IngredientGridProps> = ({
               alignItems: "center",
             }}
           >
-            <img
-              src={image}
-              alt={ingredient[index]}
-              style={{ width: "100%" }}
-            />
-            <Typography variant="subtitle1">
+            <img src={image} alt={ingredient[index]} style={ingredientStyle} />
+            <Typography
+              variant="subtitle1"
+              color="white"
+              fontFamily={"Gloria Hallelujah"}
+            >
               {ingredient[index]} ({ingredientMeasure[index]})
             </Typography>
           </div>
@@ -39,6 +48,23 @@ const IngredientGrid: React.FC<IngredientGridProps> = ({
       ))}
     </Grid>
   );
+};
+
+const styles = {
+  paperContainer: {
+    backgroundImage: `url(${Image})`,
+    backgroundSize: "cover",
+    backgroundAttachement: "fixed",
+  },
+};
+
+const paperStyle = {
+  paperContainer: {
+    backgroundColor: grey[900] + "CA",
+    minWidth: "fit-content",
+    textAlign: "center",
+    borderRadius: "20px",
+  },
 };
 
 export default function DrinkPage() {
@@ -66,39 +92,99 @@ export default function DrinkPage() {
 
   console.log(id);
   return (
-    <Box height="100vh">
-      <AppBarElementNoSearch />
-      <Box display="flex" marginTop="200px">
-        <Box
-          display="flex"
-          marginLeft="50px"
-          marginRight="50px"
-          justifyContent="flex-start"
-          alignItems="flex-start"
-        >
-          <img style={{ borderRadius: "25px" }} src={data.cocktailImage} />
-        </Box>
-        <div>
-          <Typography variant="h2">{data.cocktailName}</Typography>
-
-          <Box marginTop="20px">
-            <Box marginTop="20px" marginBottom="20px">
-              <Typography variant="h4">Instructions:</Typography>
-              <Typography variant="h5">{data.instructions}</Typography>
-            </Box>
-            <Box marginBottom="25px">
-              <Typography variant="h4">Ingredients: </Typography>
-              <IngredientGrid
-                ingredientImage={data.ingredientImages}
-                ingredient={data.ingredients}
-                ingredientMeasure={data.ingredientMeasures}
-              />
-            </Box>
-            <Typography variant="h5">Tags:</Typography>
-            <Typography variant="h6">{data.tags}</Typography>
+    <Paper style={styles.paperContainer}>
+      <Box height="100vh" marginBottom="2%">
+        <AppBarElementNoSearch />
+        <Box display="flex" marginTop="100px">
+          <Box
+            display="flex"
+            marginLeft="2%"
+            marginRight="2%"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            marginTop="2%"
+          >
+            <Grid container direction="column" spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <Paper variant="outlined" style={paperStyle.paperContainer}>
+                  <img
+                    style={{ borderRadius: "25px" }}
+                    src={data.cocktailImage}
+                  />
+                  <Typography
+                    variant="h5"
+                    color="white"
+                    fontFamily={"Gloria Hallelujah"}
+                  >
+                    Tags:
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    color="white"
+                    fontFamily={"Gloria Hallelujah"}
+                  >
+                    {data.tags}
+                  </Typography>
+                </Paper>
+              </Grid>
+            </Grid>
           </Box>
-        </div>
+
+          <Box marginLeft="2%" marginTop="2%" marginRight="5%">
+            <Paper
+              variant="outlined"
+              style={{
+                ...paperStyle.paperContainer,
+                backgroundColor: grey[200],
+              }}
+            >
+              <Typography
+                variant="h1"
+                color="black"
+                fontFamily={"Gloria Hallelujah"}
+                fontWeight="bold"
+              >
+                {data.cocktailName}
+              </Typography>
+            </Paper>
+            <Box marginTop="3%">
+              <Paper variant="outlined" style={paperStyle.paperContainer}>
+                <Typography
+                  variant="h3"
+                  color="white"
+                  fontFamily="WalterTurncoat"
+                  fontWeight="bold"
+                >
+                  Instructions:
+                </Typography>
+                <Typography
+                  variant="h4"
+                  color="white"
+                  fontFamily={"WalterTurncoat"}
+                >
+                  {data.instructions}
+                </Typography>
+              </Paper>
+            </Box>
+          </Box>
+        </Box>
+        <Box marginLeft="2%" marginTop="2%" flexGrow={1} marginRight="5%">
+          <Paper variant="outlined" style={paperStyle.paperContainer}>
+            <Typography
+              variant="h4"
+              color="white"
+              fontFamily={"Gloria Hallelujah"}
+            >
+              Ingredients:
+            </Typography>
+            <IngredientGrid
+              ingredientImage={data.ingredientImages}
+              ingredient={data.ingredients}
+              ingredientMeasure={data.ingredientMeasures}
+            />
+          </Paper>
+        </Box>
       </Box>
-    </Box>
+    </Paper>
   );
 }
